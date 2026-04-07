a336ffe1393a41551701c4268951060a9c0c6845
naveenkumarl89 <106944811+naveenkumarl89@users.noreply.github.com>
2026-04-07 13:29:27 +0530

    Define ClientConfig struct in config.h
    Added ClientConfig struct with default values for server URL, endpoint name, PSK ID, and PSK key.

--- examples/config.h
+++ examples/config.h
@@ -1,1 +1,12 @@
+#pragma once
+#include <string>
 
+struct ClientConfig {
+    std::string serverUrl   = "coap://deviosfriendlytech.com:5683";
+    std::string endpointName = "Navs";
+    std::string pskId       = "";
+    std::string pskKey      = "";
+};
+
+// Global instance, set by main(), read by objects.cpp
+extern ClientConfig g_config;
